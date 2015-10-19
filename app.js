1779e5be4ce2758515b437834b3aabff86c0157e
Igor1088 <igor.puzovic@yahoo.com>
2015-10-19 18:41:34 +0200

    replace jquery with javascript

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 
-$(document).ready( function(){
-	generateQuotes();
+document.addEventListener("DOMContentLoaded", function(){
+	showQuotes();
+	// generate quotes
+	function showQuotes(){
+		var quotes = [ { quote: "Nothing will come of nothing; we must dare mighty things.", author: "William Shakespeare"},
+		 { quote: "Choose a job you love, and you will never have to work a day in your life ", author: "Confucius"},
+		 { quote: "Only one who devotes himself to a cause with his whole strength and soul can be a true master. For this reason mastery demands all of a person.", author: "Albert Einstein"}
+		 ];
+		 // get random quote
+		var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+		document.querySelector(".quote").innerHTML = randomQuote.quote;
+		document.querySelector(".author").innerHTML = randomQuote.author;
+	
 
- function generateQuotes(){
-    var quotes = ["William Shakespeare: Nothing will come of nothing; we must dare mighty things.","Albert Einstein: The person who reads too much and uses his brain too little will fall into lazy habits of thinking.","Confucius: Choose a job you love, and you will never have to work a day in your life ","Tolstoy: The strongest of all warriors are these two Time and Patience."];
+	}
+	// show quote after button is clicked
+	document.querySelector(".button").addEventListener("click", function(){
+			showQuotes();
+		}, false);
 
-    
-    var random = quotes[Math.floor(Math.random() * quotes.length)];
-    quoteSplit = random.split(":"); 
-    $('.author').text(quoteSplit[0]);
-    $('.quote').text(quoteSplit[1]);
-        
-    
-}
 
-	$('.button').on("click", function(){
-		generateQuotes();
-	})
-});
+}, false);
 
